5944c9707e305ae99e16302e737611c278c86793
interfiberschool <117294755+interfiberschool@users.noreply.github.com>
2025-11-19 13:58:26 +0000

    doc: v5dbg.mk

--- v5dbg.mk
+++ v5dbg.mk
@@ -1,9 +1,15 @@
 # V5dbg build configuration
 # Makefiles are shit... 11/18/25
 
-# Enable v5dbg? When disabled .stub.h files are included to overwrite macros
-ENABLE_DEBUGGER := false
+# About: When 'true' debugger functionality is enabled, when 'false' its disabled
+# Cont: Enables the including of .stub.h files to replace debugger functionality
+# Note: Enable this for a final competition build!
+ENABLE_DEBUGGER := true
 
 ######## Apply configuration, DO NOT MODIFY ########
 
 EXTRA_CXXFLAGS += $(if $(filter $(ENABLE_DEBUGGER),true),"",-DV5DBG_DISABLE)
+
+stub:
+	@echo "Calling stub compiler..."
+	@python3 stub.py
